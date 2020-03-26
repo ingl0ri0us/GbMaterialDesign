571133e106b0330aba2507609ab84315353d0af2
ingl0ri0us <rinat.y@protonmail.com>
2020-03-26 09:48:19 +0300

    lesson 8 homework

---
+++ app/src/main/java/com/ingl0ri0us/gbmaterialdesign/ui/StyleActivity.kt
@@ -0,0 +1,13 @@
+package com.ingl0ri0us.gbmaterialdesign.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ingl0ri0us.gbmaterialdesign.R
+
+class StyleActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_style)
+    }
+}
